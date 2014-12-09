0af9c299f275b6e2e9c35975b8c4c8fa22a2dd6f
qbi11y <qbi11y@gmail.com>
2014-12-09 13:17:58 -0600

    12/9/2014-1317
    Form binding

--- js/script.js
+++ js/script.js
@@ -5,6 +5,17 @@
         return {message: "message from a factory"}
     })
     app.controller('TutController', ['$scope', 'Data', function($scope, Data){
+        $scope.master =  {};
+
+        $scope.update = function(user) {
+            $scope.master = angular.copy(user);
+        };
+
+        $scope.reset = function() {
+            $scope.user = angular.copy($scope.master);
+        };
+
+        $scope.reset();
         $scope.test = "this is a test"
         $scope.data = Data.message;
 
