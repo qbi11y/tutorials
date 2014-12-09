441773cde9c3c94bce40b231bf908871a6f19282
qbi11y <qbi11y@gmail.com>
2014-12-09 11:52:58 -0600

    12/9/2014-1152

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
-function FirstController($scope) {
-    $scope.data = {message: "Hello"}
-}
+(function(){
+    var app = angular.module('tutorial', []);
+
+    app.factory('Data', function() {  //this is a service
+        return {message: "message from a factory"}
+    })
+    app.controller('TutController', ['$scope', 'Data', function($scope, Data){
+        $scope.test = "this is a test"
+        $scope.data = Data.message;
+
+        $scope.myMethod = function() {
+            return "this is a method"
+        }
+    }]);
+
+})();
